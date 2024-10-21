84e7a69c6443ba383ccdf2f8ad34f2535b6d6664
vnieblas <valeria.nieblas01@estudiant.upf.edu>
2024-10-21 17:44:06 +0200

    First attempt

--- src/antiprime.py
+++ src/antiprime.py
@@ -4,8 +4,27 @@
 def main() :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
-
-
+	i = 1
+	j = 0
+	while i <= x:
+		if x % i == 0:
+			j = j + 1
+		i = i + 1
+	l = x - 1
+	k = 0
+	while l >= 1 and k < j:
+		a = 1
+		k = 0
+		while a <= l:
+			if l % a == 0:
+				k = k + 1
+			a = a + 1
+		l = l - 1
+	if k >= j:
+		res = "not anti-prime"
+		return("not anti-prime")
+	else:
+		res = "anti-prime"
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
@@ -15,7 +34,9 @@ def main() :
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
-
+	import sys
+	if len(sys.argv) > 1:
+		x = int(sys.argv[1])
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
 	## $ python antiprime.py 6
